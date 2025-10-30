aa99fdd231962390154f7e6970a2b728434c82ed
Kirillkonder <tehnoblogery754@gmail.com>
2025-10-30 20:57:46 +0300

    1.0.1

--- server.py
+++ server.py
@@ -64,5 +64,8 @@ def get_latest_screen():
         return send_file(latest_screen, mimetype='image/jpeg')
     return 'No image'
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
